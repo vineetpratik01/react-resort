fde23f3df322682b4282ceb9aeebdace9acf84a9
shreya750 <shreyakumar71@.gmail.com>
2021-08-07 14:02:00 +0530

    filterRooms functionality added

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from 'react';
+import{useContext} from 'react';
+import { RoomContext } from '../Context';
+import Title from '../components/Title'
 
-export default function RoomFilter() {
+//get all unique values
+const getUnique = (items,value) => {
+    return [...new Set(items.map(item => item[value]))]
+}
+
+export default function RoomFilter({rooms}) {
+    const context = useContext(RoomContext) //from which part of room context
+    //console.log(context)
+    const {
+        handleChange,
+        type,
+        capacity,
+        price,
+        minPrice,
+        maxPrice,
+        minSize,
+        maxSize,
+        breakfast,
+        pets
+    } = context ;
+
+    //get unique types
+    let types = getUnique(rooms,'type');
+    types= ['all',...types];
+
+    //map to jsx
+    types= types.map((item,index) => {
+        return <option value={item} key={index}>{item}</option>
+    })
     return (
-        <div>
-            hello from room filter
-        </div>
+        <section className='filter-container'>
+            <Title title="search rooms"/>
+            <form className='filter-form'>
+                {/* select type */}
+                <div clas="form-group">
+                    <label htmlFor="type">
+                        room type
+                    </label>
+                    <select 
+                    name="type"
+                    id="type"
+                    value={type}
+                    className="form-control"
+                    onChange={handleChange}
+                    >
+                    {types}
+
+                    </select>
+                </div>
+                {/* end select type */}
+            </form>
+        </section>
     )
 }
